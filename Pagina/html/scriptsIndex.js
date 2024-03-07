f543ba1803ec19a6cdcf2bbe5ffba1f4575f4c31
SErgi <sergimartinez16@gmail.com>
2024-03-07 17:45:33 +0100

    02

--- Pagina/html/scriptsIndex.js
+++ Pagina/html/scriptsIndex.js
@@ -1,7 +1,17 @@
-const botonCambiarTema = document.getElementById('cambiarTema');
-const body = document.body;
+// cambio de tema de claro a oscuro y viceversa y cambiar el texto del boton
+var toggleThemeButton = document.getElementById('toggle-theme-btn');
 
-botonCambiarTema.addEventListener('click', () => {
-    // Cambiar entre los temas claro y oscuro
-    body.classList.toggle('tema-claro');
+
+toggleThemeButton.addEventListener('click', function() {
+  
+    document.body.classList.toggle('tema-oscuro');
+   
+    var isDarkMode = document.body.classList.contains('tema-oscuro');
+    
+
+    if (isDarkMode) {
+        toggleThemeButton.textContent = 'Tema claro';
+    } else {
+        toggleThemeButton.textContent = 'Tema oscuro';
+    }
 });
