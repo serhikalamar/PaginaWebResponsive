2d87fe4c44a57db20ce113faf1593f507aff0b79
SErgi <sergimartinez16@gmail.com>
2024-03-12 16:54:52 +0100

    not bad

--- Pagina/html/slider.js
+++ Pagina/html/slider.js
@@ -1,11 +1,12 @@
-let sliderInner = document.querySelector(".slider--inner");
-let index = 1;
-let imagenes = sliderInner.querySelectorAll("img");
+let sliderInner = document.querySelector(".slider--inner"); //selecionamos la etiqueta div con esa clase.
+let index = 1; //necesitamos saber que index llevamos
+let imagenes = sliderInner.querySelectorAll("img"); //
+//creamos un intervalo que va transforma el div en la posicion de la imagen y va sumando hasta llegar al ultimo y vuelve a empezar
 setInterval(function(){
     let porcentaje = index * - 100;
     sliderInner.style.transform = "translateX(" + porcentaje +"%)";
     index++;
-    if(index > (imagenes.length - 1)){
+    if(index > (imagenes.length - 2)){
         index = 0;
     }
 },3000);
